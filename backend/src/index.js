a658a6475352f972e4a987ade4b085be1110e8ea
markian.ivanichok <markian.ivanichok@gmail.com>
2016-06-23 22:51:11 +0300

    Added Access-Control-Allow-Origin headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,11 @@ const app = express()
 function configurateExpress(app){
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 }
 
 const connectDataBase = uri => {
